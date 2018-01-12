abaf975f906317ebbdfe3739da04cc54004d3427
Vlad Eugeniu Stefan <stefan.vlad1996@yahoo.com>
2018-01-12 20:24:31 +0000

    mare push, sorry for this

--- BooksReviewApp/urls.py
+++ BooksReviewApp/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     
+    url(r'^SignUp/$', views.SignUpView.as_view(), name='create-user'),
+    url(r'^SignIn/$', views.SignInView.as_view(), name='log-user'),
+    
 ]
---
+++ BooksReviewApp/views/vlad_views.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic import    View, TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .forms import LoginForm 
+
+class SignUpView ( CreateView ) :    
+    form_class = UserCreationForm
+    template_name = 'BooksReviewApp/signup.html'
+    success_url = reverse_lazy( 'SignIn' )
+    
+    
+class SignInView(View):
+    
+    def get(self, request, *args, **kwargs):
+        form = LoginForm()
+        return render(request, 'BooksReviewApp/signin.html',{'form':form })    
+    
+    def post(self, request, *args, **kwargs):
+        context = {}
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            user = authenticate(username=form.cleaned_data['username'],
+                                password=form.cleaned_data['password'])
+            if user:
+                login(request=request,
+                      user=user)
+                return redirect('BooksReviewApp/home')
+            else:
+                context['error_message'] = 'Wrong username or password!'
+        form = LoginForm()
+        context['form'] = form
+        context['error'] = 1
+        return render(request, 'BooksReviewApp/signin.html', context)
