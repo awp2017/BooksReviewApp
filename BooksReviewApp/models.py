a035ecf194d9088fe78f9002e11f7b968f7ebc12
Stefan Chituc <stefan.chituc@my.fmi.unibuc.ro>
2018-01-12 16:02:18 +0000

    Book and Writer models added

--- BooksReviewApp/models.py
+++ BooksReviewApp/models.py
@@ -3,4 +3,19 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class Writer(models.Model):
+    name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
+       
+    class Meta:
+        ordering = ('name',)
+    
+        
+class Book(models.Model):
+    name = models.CharField(max_length=30)
+    writers = models.ManyToManyField(Writer)
+    
+    def __str__(self):
+        return self.name
