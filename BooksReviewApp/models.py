d496bd18abffa44d733c02feb145df9b865e6953
danodobescu <dan.odobescu@gmail.com>
2018-01-13 08:50:40 +0000

    Add models

--- BooksReviewApp/models.py
+++ BooksReviewApp/models.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.contrib.auth.models import User
+from django.utils import timezone
 from django.db import models
 
 class Writer(models.Model):
@@ -11,7 +12,6 @@ class Writer(models.Model):
        
     class Meta:
         ordering = ('name',)
-    
         
 class Book(models.Model):
     name = models.CharField(max_length=30)
@@ -28,3 +28,21 @@ class BookRequest(models.Model):
     
     def __str__(self):
         return self.author.username + " asks for: " + self.writer + " " + self.book_name
+
+class Review(models.Model):
+    author_pk = models.ForeignKey('auth.User')
+    book_pk = models.ForeignKey(Book, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    review_text = models.CharField(max_length=1000)
+    
+    def __str__(self):
+        return self.review_text
+
+class Comment(models.Model):
+    user_pk = models.ForeignKey('auth.User')
+    review_pk = models.ForeignKey(Review, on_delete=models.CASCADE)
+    comm_text = models.CharField(max_length=500)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.comm_text
