003715177db35cb0a4829efecd8a81e9dd147b85
Vlad Eugeniu Stefan <stefan.vlad1996@yahoo.com>
2018-01-13 08:35:33 +0000

    Request model and migrations

--- BooksReviewApp/models.py
+++ BooksReviewApp/models.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.contrib.auth.models import User
 from django.db import models
 
 class Writer(models.Model):
@@ -19,3 +19,12 @@ class Book(models.Model):
     
     def __str__(self):
         return self.name
+
+class BookRequest(models.Model):
+    author =  models.ForeignKey(User, on_delete=models.CASCADE)
+    writer = models.CharField(max_length=50)
+    book_name = models.CharField(max_length=50)
+    date = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.author.username + " asks for: " + self.writer + " " + self.book_name
